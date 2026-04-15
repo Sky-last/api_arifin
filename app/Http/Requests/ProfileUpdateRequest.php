9d0b3795da2f4f5ff0763562d4f7b8a1128f6531
kaitama <mr.ibnutama@gmail.com>
2026-04-15 22:18:05 +0700

    First commit

---
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\User;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class ProfileUpdateRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'username' => [
+                'required',
+                'string',
+                'min:6',
+                Rule::unique(User::class)->ignore($this->user()->id)
+            ],
+            'photo_path' => [
+                'nullable',
+                'file',
+                'mimes:png,jpg,jpeg',
+                'max:1024',
+            ],
+            'address' => 'required',
+            // lanjutkan untuk update data profil (foto dll)....
+            'email' => [
+                'required',
+                'string',
+                'lowercase',
+                'email',
+                'max:255',
+                Rule::unique(User::class)->ignore($this->user()->id),
+            ],
+        ];
+    }
+}
